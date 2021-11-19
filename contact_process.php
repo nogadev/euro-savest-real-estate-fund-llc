b76d2e5b0944e8bc410b907f9f998be0c376ed61
Franco Caballero <caballerofranco12@gmail.com>
2021-11-19 14:35:25 -0300

    Cambios en configuracion de contact_process

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "marklm@eurusavest.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
@@ -13,9 +13,9 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $subject = "You have a message from Eurusavest Real State Fund LLC.";
 
-    $logo = 'img/logo.png';
+    $logo = 'img/logo2.png';
     $link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
